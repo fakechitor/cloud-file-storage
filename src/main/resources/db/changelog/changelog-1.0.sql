18271d31891858af56acc34685cf1cdd5c6b88c4
fakechitor <ladasedanbaklajan06@gmail.com>
2025-02-09 17:17:38 +0300

    feat(liquibase): add migration for users table

---
+++ src/main/resources/db/changelog/changelog-1.0.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+
+--changeset fakechitor:1
+CREATE SEQUENCE user_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    CACHE 1;
+
+--changeset fakechitor:2
+CREATE TABLE users (
+   id BIGINT PRIMARY KEY DEFAULT nextval('user_id_seq'),
+   login VARCHAR(30) UNIQUE NOT NULL,
+   password VARCHAR(30) NOT NULL
+       CHECK (length(password) >= 3 AND length(password) <= 30)
+);
