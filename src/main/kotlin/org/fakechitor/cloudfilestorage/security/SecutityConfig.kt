98de46c2bc5e2044f710de092748dae4b4be360d
fakechitor <ladasedanbaklajan06@gmail.com>
2025-02-20 04:39:32 +0300

    feat(config): remove context-path in application.yml

---
+++ src/main/kotlin/org/fakechitor/cloudfilestorage/security/SecutityConfig.kt
@@ -0,0 +1,99 @@
+package org.fakechitor.cloudfilestorage.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.SecurityContextHolderStrategy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository
+import org.springframework.security.web.context.SecurityContextRepository
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+class SecurityConfig(
+    private val userDetailsService: UserDetailsService,
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            csrf { disable() }
+            authenticationManager
+            securityContext {
+                securityContextRepository = securityContextRepository()
+            }
+            securityMatcher("/api/**")
+            authorizeHttpRequests {
+                authorize(HttpMethod.POST, "/api/auth/**", permitAll)
+                authorize(HttpMethod.GET, "/api/admin/**", hasRole("ADMIN"))
+                authorize(anyRequest, authenticated)
+            }
+            logout {
+                logoutUrl = "/api/auth/logout"
+                invalidateHttpSession = true
+                clearAuthentication = true
+                deleteCookies("JSESSIONID")
+                logoutSuccessHandler = logoutSuccessHandler()
+            }
+            exceptionHandling {
+                authenticationEntryPoint = authenticationEntryPoint()
+                accessDeniedHandler = accessDeniedHandler()
+            }
+        }
+
+        return http.build()
+    }
+
+    @Bean
+    fun authenticationProvider() =
+        DaoAuthenticationProvider().apply {
+            setUserDetailsService(userDetailsService)
+            setPasswordEncoder(passwordEncoder())
+        }
+
+    @Bean
+    fun authenticationManager(configuration: AuthenticationConfiguration): AuthenticationManager = configuration.authenticationManager
+
+    @Bean
+    fun securityContextRepository(): SecurityContextRepository = HttpSessionSecurityContextRepository()
+
+    @Bean
+    fun securityContextHolder(): SecurityContextHolderStrategy = SecurityContextHolder.getContextHolderStrategy()
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationEntryPoint(): AuthenticationEntryPoint =
+        AuthenticationEntryPoint { request, response, exception ->
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+        }
+
+    @Bean
+    fun accessDeniedHandler(): AccessDeniedHandler =
+        AccessDeniedHandler { request, response, exception ->
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied")
+        }
+
+    @Bean
+    fun logoutSuccessHandler(): LogoutSuccessHandler =
+        LogoutSuccessHandler { request: HttpServletRequest?, response: HttpServletResponse, authentication: Authentication? ->
+            response.status = 204
+            response.contentType = "application/json"
+        }
+}
