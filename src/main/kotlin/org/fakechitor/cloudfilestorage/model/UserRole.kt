42869d4d464a68aa4e60400dae648918d5cb5cb0
fakechitor <ladasedanbaklajan06@gmail.com>
2025-02-20 05:17:17 +0300

    refactor(entity): make secondary constructor to primary

--- src/main/kotlin/org/fakechitor/cloudfilestorage/model/UserRole.kt
+++ src/main/kotlin/org/fakechitor/cloudfilestorage/model/UserRole.kt
@@ -4,22 +4,15 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "user_roles")
-class UserRole {
+class UserRole(
+    @ManyToOne
+    @JoinColumn(name = "id_user", nullable = false) var user: User?,
+    @ManyToOne
+    @JoinColumn(name = "id_role", nullable = false) var role: Role?
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_roles_id_seq")
     @SequenceGenerator(name = "user_roles_id_seq", sequenceName = "user_roles_id_seq", allocationSize = 1)
     var id: Long? = null
 
-    @ManyToOne
-    @JoinColumn(name = "id_user", nullable = false)
-    var user: User? = null
-
-    @ManyToOne
-    @JoinColumn(name = "id_role", nullable = false)
-    var role: Role? = null
-
-    constructor(user: User?, role: Role?) {
-        this.user = user
-        this.role = role
-    }
 }
