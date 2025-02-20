6ab8a2492b6f2696f883df8c8d0229602e4689ca
fakechitor <ladasedanbaklajan06@gmail.com>
2025-02-20 04:47:07 +0300

    feat(security): add UserDetailsService

---
+++ src/main/kotlin/org/fakechitor/cloudfilestorage/security/UserDetailsService.kt
@@ -0,0 +1,31 @@
+package org.fakechitor.cloudfilestorage.security
+
+import org.fakechitor.cloudfilestorage.repository.CustomRepository
+import org.fakechitor.cloudfilestorage.repository.UserRepository
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class UserDetailsService(
+    private val userRepository: UserRepository,
+    private val customRepository: CustomRepository,
+) : UserDetailsService {
+    override fun loadUserByUsername(username: String?): UserDetails {
+        val user = username?.let { userRepository.findByLogin(it) } ?: throw UsernameNotFoundException("User not found")
+        return User(
+            user.login,
+            user.password,
+            getGrantedAuthorities(user.login),
+        )
+    }
+
+    private fun getGrantedAuthorities(username: String?): List<GrantedAuthority> =
+        customRepository.getAuthoritiesByName(username).map {
+            SimpleGrantedAuthority(it)
+        }
+}
