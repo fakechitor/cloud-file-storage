8089487565d111c52455e3002de83adf724aa8d1
fakechitor <ladasedanbaklajan06@gmail.com>
2025-02-28 10:27:02 +0300

    refactor(minio): remove unused methods/properties

--- src/main/kotlin/org/fakechitor/cloudfilestorage/service/MinioService.kt
+++ src/main/kotlin/org/fakechitor/cloudfilestorage/service/MinioService.kt
@@ -1,40 +1,16 @@
 package org.fakechitor.cloudfilestorage.service
 
 import io.minio.*
-import io.minio.messages.Bucket
 import org.springframework.core.io.InputStreamResource
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
 
 @Service
 class MinioService(
     private val minioClient: MinioClient,
 ) {
     companion object {
-        const val SLASH = "/"
         const val HOME_BUCKET = "user-files"
-        const val EMPTY_STRING = ""
-    }
-
-    fun getAllBuckets(): MutableList<Bucket> = minioClient.listBuckets()
-
-    fun uploadBucket(
-        bucketName: String,
-        file: MultipartFile,
-    ) {
-        minioClient.putObject(
-            PutObjectArgs
-                .builder()
-                .bucket(bucketName)
-                .`object`(file.name)
-                .stream(
-                    file.inputStream,
-                    file.size,
-                    -1,
-                ).contentType(file.contentType)
-                .build(),
-        )
     }
 
     fun downloadFile(
