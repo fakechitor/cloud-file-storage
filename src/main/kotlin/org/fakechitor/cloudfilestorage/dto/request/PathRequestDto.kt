0e562e15a641e9bd52f5f88b20f139c523cd8a4e
fakechitor <ladasedanbaklajan06@gmail.com>
2025-02-28 16:18:23 +0300

    fix(minio): delete validation on pathDto

--- src/main/kotlin/org/fakechitor/cloudfilestorage/dto/request/PathRequestDto.kt
+++ src/main/kotlin/org/fakechitor/cloudfilestorage/dto/request/PathRequestDto.kt
@@ -3,6 +3,6 @@ package org.fakechitor.cloudfilestorage.dto.request
 import jakarta.validation.constraints.Size
 
 data class PathRequestDto(
-    @field:Size(min = 3, max = 63, message = "Path length must between 3 and 63")
+    @field:Size(max = 63, message = "Path length must between 3 and 63")
     val path: String?,
 )
