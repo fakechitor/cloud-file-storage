f4708c911ee2d84f2ea7034194b47cfd8d6652b1
fakechitor <ladasedanbaklajan06@gmail.com>
2025-03-10 16:02:13 +0300

    feat(swagger): add documentation for user controller

--- src/main/kotlin/org/fakechitor/cloudfilestorage/controller/UserController.kt
+++ src/main/kotlin/org/fakechitor/cloudfilestorage/controller/UserController.kt
@@ -1,5 +1,6 @@
 package org.fakechitor.cloudfilestorage.controller
 
+import org.fakechitor.cloudfilestorage.docs.users.UserInfoDocs
 import org.fakechitor.cloudfilestorage.service.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*
 class UserController(
     private val userService: UserService,
 ) {
+    @UserInfoDocs
     @GetMapping("/me")
     fun getPersonalInfo() = ResponseEntity.ok(userService.getUsernameIfAuthorized())
 }
